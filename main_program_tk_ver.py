dc22e7c087a518e8fbfdead83b97a4763f916c75
AntonioHollerman <toniohollerProf@gmail.com>
2023-11-17 14:07:40 -0500

    Create executable

--- main_program_tk_ver.py
+++
@@ -1,5 +0,0 @@
-from tk_buttons_functions import *
-
-app_window = PasswordManager()
-app_window.title("Password Manager App")
-app_window.start_program()
---
+++ password_manager.py
@@ -0,0 +1,7 @@
+from tk_buttons_functions import *
+
+app_window = PasswordManager()
+app_window.title("Password Manager App")
+app_window.start_program()
+
+# Python -m PyInstaller password_manager.py --windowed --onefile
